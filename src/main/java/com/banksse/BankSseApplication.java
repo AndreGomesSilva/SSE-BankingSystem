7ae2258d7af934d257c8bb1fdb9e6c02c7941ea6
andre-gomes-silva <andre.gomesdasilva@thoughtworks.com>
2025-09-10 14:54:06 -0300

    Implement Saving Account: - withdraw limit - rate interest - number account - counter number of saving accounts

--- src/main/java/com/banksse/BankSseApplication.java
+++ src/main/java/com/banksse/BankSseApplication.java
@@ -1,6 +1,7 @@
 package com.banksse;
 
 import com.banksse.service.CheckingAccount;
+import com.banksse.service.SavingAccount;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,9 +12,10 @@ public class BankSseApplication {
     public static void main(String[] args) {
         SpringApplication.run(BankSseApplication.class, args);
 
-        CheckingAccount conta = new CheckingAccount(new BigDecimal(250.50), 1540);
-        conta.printAccount();
+        CheckingAccount checkingAccount = new CheckingAccount(new BigDecimal(250.50), 1540);
+        checkingAccount.printAccount();
 
-
+        SavingAccount savingAccount = new SavingAccount(new BigDecimal(5000.00), 1540, new BigDecimal("0.05"));
+        savingAccount.printAccount();
     }
 }
